8163f72eac79a957952fe85e71186e756650618a
juhun <minjuhun1@gmail.com>
2025-11-15 15:33:28 +0900

    init: Project init - Tailwind CSS install - Eslint, Prettier setting & Taliwind 플러그인 적용 - Zustand install & 기본세팅 - Tanstack install & Provider 구성 - Shadcn UI install - Typescript 옵션 파일 조정

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router";
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>home</div>} />
+      <Route path="/" element={<div>home</div>} />
+    </Routes>
+  );
+}
